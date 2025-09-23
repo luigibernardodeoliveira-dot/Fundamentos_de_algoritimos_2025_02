38eb8469bea1d30ce74a22b8ad6a1f26cc069b0d
LUIGI OLIVEIRA <luigibernardodeoliveira@gmail.com>
2025-09-23 15:46:08 -0300

    aula_07ex05

---
+++ aula_07_at_8.py
@@ -0,0 +1,6 @@
+def area_triangulo(base, altura):
+    return base * altura / 2
+
+print("area do triangulo = ", area_triangulo(5, 10))
+print("area do triangulo = ", area_triangulo(1, 2))
+print("area do triangulo = ", area_triangulo(4, 6))
