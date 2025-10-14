f9a8d0f66e50fa65876101a67de76d83ec506e07
LUIGI OLIVEIRA <luigibernardodeoliveira@gmail.com>
2025-10-14 16:26:35 -0300

    Atualiazando_feifood

--- FEIFOOD.py
+++ FEIFOOD.py
@@ -1,13 +1,30 @@
-def login(email,senha):
-    print ("aqui vou fazer a funçao loguin")
+def menu_principal():
+        while True:
+            print("SEJA BEM VINDO AO FEIFOOD")
+            print("1 - Cadastrar usuário")
+            print("2 - Login")
+            print("0 - Sair")
+            opcao = input("Escolha: ")
 
-def cadastro(nome_usuario,email,senha,data_nasc):
-    print("aqui vou fazr a funçao do cadratro")
+            if opcao == "1":
+                cadastrar_usuario()
+            elif opcao == "2":
+                login_usuario()
+            elif opcao == "0":
+                break
+            else:
+                print("Opção inválida!")
+
+        def cadastrar_usuario(nome_usuario,email,senha,data_nasc):
+
+            def login(email,senha):
+    
+
 
 def menu_principal():
     print("lista de alimentos")
 
-def porçao(media, pequena, grande):
+def porcao(media, pequena, grande):
     print("tamanho")
 
 def preco():
